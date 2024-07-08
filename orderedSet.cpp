2341329796e5e3f256ee195cb9948aa23c896a1d
Ritwij Kumar <136425629+LockYourSoul@users.noreply.github.com>
2024-07-08 10:36:27 +0530

    Update orderedSet.cpp

--- orderedSet.cpp
+++ orderedSet.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; // find_by_order, order_of_key
 
 int main(){
     //defining a policy based data structure
-    pbds s;
+    ordered_set s;
     s.insert(3);
     s.insert(100);
     s.insert(203);
@@ -18,9 +18,9 @@ int main(){
     cout << s.order_of_key(5) << endl;
     cout<<*s.find_by_order(2)<<endl;
     
-    //order_of_key(x) returns number of elements strictly less than x in the set ;
+    //order_of_key(x) returns number of elements strictly less than x in the set
     //find_by_order(x) returns the element at xth position
-    // you can also use functions that are used in unordered set
+    //you can also use functions that are used in unordered set
     //NOTE: Both of these operations take O(logN) time
     return 0;
 }
